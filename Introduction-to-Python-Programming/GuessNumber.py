dc291d417b292ba993979547ee07adb176dd6453
prajakta gujarathi <prajgujarathi@gmail.com>
2016-06-13 15:52:23 -0500

    Update GuessNumber.py

--- Introduction-to-Python-Programming/GuessNumber.py
+++ Introduction-to-Python-Programming/GuessNumber.py
@@ -6,48 +6,75 @@
 import simplegui
 import random
 number = 0
+count = 7
+range1 = ""
 # helper function to start and restart the game
 def new_game():
     # initialize global variables used in your code here
+    global number ,count 
     number = 0
-    frame.start()
+    count = 7
+    
+    if(range1 != ""):
+        if(range1 == "range100"):
+            range100()
+        elif(range1 == "range1000"):
+            range1000()  
+    else:
+        range100()
+        
+
 
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
-    global number
+    global number, count , range1
+    range1 = "range100"
+    count = 7
     number = random.randrange(0,100,1)
-    print number
+    print "Guess number in Range 100"
     return number
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game     
-    global number
+    global number, count , range1 
+    range1 = "range1000"
+    count = 10
     number = random.randrange(0,1000,1)
+    print "Guess number in Range 1000"
     return number
     
 def input_guess(guess):
     # main game logic goes here	
-    global number
-    if(guess > number):
-        print "Lower"
-        return
-    if(guess < number):
-        print "Higher"
-        return
-    if(guess == number):
-        print "Correct"
-        return
+    global count
+    print "Number of guess remaining are "+ str(count)
+    if(count >= 0):
+        count -= 1
+        print "Guess was: " + guess
+        global number
+        if(int(guess) > number):
+            print "Lower"
+            return
+        if(int(guess) < number):
+            print "Higher"
+            return
+        if(int(guess) == number):
+            print "Correct"
+            new_game()
+    else:
+        print "You are out of Guess. Start new Game"
+        new_game()
     
 
     
 # create frame
-frame = simplegui.create_frame('Input_guess', 500, 500)
+frame = simplegui.create_frame('Input_guess', 200, 200)
 inp = frame.add_input('Guess', input_guess, 50)
 button1 = frame.add_button('range100', range100 ,100)
 button2 = frame.add_button('range1000', range1000, 100)
-
+frame.add_button('Restart',new_game,100)
+frame.start()
 # register event handlers for control elements and start frame
 
 
